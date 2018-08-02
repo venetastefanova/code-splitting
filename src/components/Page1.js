9e533b3a3c5e186beef3127d0cdf95b281034277
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-08-02 16:27:04 +0300

    initial setup

---
+++ src/components/Page1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+
+const Page1 = () => {
+    <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+    <button onClick={}>Page2</button>
+    <button onClick={}>Page3</button>
+    </div>
+}
