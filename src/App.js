5fcb2c8659e3495c38b4732c47bb58b59ac5c53e
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-08-02 17:14:24 +0300

    implementing code splitting by importing files only when needed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
+
 
 class App extends Component {
   state ={
-    route :'page1'
+    route :'page1',
+    component:''
   }
   
   onRouteChange=(route)=>{
-    this.setState({route:route});
+    //this.setState({route:route});
+    if(route==='page1'){
+      this.setState({route:route})
+    } 
+    else if(route==='page2'){// import page2, return a promise and update the state
+        import('./components/Page2').then((Page2)=>{
+          // console.log(Page2);
+          this.setState({route:route, component:Page2.default}); //ading default because we export it like this
+        });
+    }
+    else if(route==='page3'){// import page3, return a promise and update the state
+      import('./components/Page3').then((Page3)=>{
+        this.setState({route:route, component:Page3.default});
+      });
+  }
   }
   render() {
 
-      if(this.state.route==='page1'){
+      // if(this.state.route==='page1'){
+      //   return  <Page1 onRouteChange={this.onRouteChange}/>
+      // } 
+      // else if(this.state.route==='page2'){
+      //     return  <Page2 onRouteChange={this.onRouteChange}/>
+      // }
+      // else{
+      //   return  <Page3 onRouteChange={this.onRouteChange}/>
+      // }
+      if(this.state.route === 'page1'){
         return  <Page1 onRouteChange={this.onRouteChange}/>
-      } 
-      else if(this.state.route==='page2'){
-          return  <Page2 onRouteChange={this.onRouteChange}/>
       }
-      else{
-        return  <Page3 onRouteChange={this.onRouteChange}/>
+      else {
+        return <this.state.component onRouteChange={this.onRouteChange}/>
       }
   }
 }
