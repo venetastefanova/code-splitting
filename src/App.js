c339b02661aa73a9e2ef6216db3917eafb0c4b4e
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-08-02 16:48:05 +0300

    adding simple routing between the 3 pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
 class App extends Component {
+  state ={
+    route :'page1'
+  }
+  
+  onRouteChange=(route)=>{
+    this.setState({route:route});
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Page1/>
-        <Page2/>
-        <Page3/>
-      </div>
-    );
+
+      if(this.state.route==='page1'){
+        return  <Page1 onRouteChange={this.onRouteChange}/>
+      } 
+      else if(this.state.route==='page2'){
+          return  <Page2 onRouteChange={this.onRouteChange}/>
+      }
+      else{
+        return  <Page3 onRouteChange={this.onRouteChange}/>
+      }
   }
 }
 
--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 
 
-const Page1 = () => {
+const Page1 = ({onRouteChange}) => (
     <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-    <button onClick={}>Page2</button>
-    <button onClick={}>Page3</button>
+    <button onClick={()=>onRouteChange('page2')}>Page2</button>
+    <button onClick={()=>onRouteChange('page3')}>Page3</button>
     </div>
-}
+);
+
+export default Page1;
